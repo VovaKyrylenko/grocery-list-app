7e9646efae65a0385e531b17ce691c9d72885ca6
Volodymyr Kyrylenko <vovichkyry@gmail.com>
2024-04-06 10:31:54 +0300

    Updated SEO and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Head from "next/head";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "@/styles";
@@ -9,20 +8,15 @@ import { ReactQueryProvider } from "@/providers/query-client-provider";
 import { NavBar } from "@/components/common/";
 import { SessionCustomProvider } from "@/providers";
 
+export const metadata: Metadata = {
+  title: "GroceryManager",
+  description: "This app aimed to help you to manage your grocery list",
+  keywords: ["grocery", "list", "shopping", "management", "food"],
+};
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <Head>
-        <title>Grocery Manager</title>
-        <meta
-          name="description"
-          content="This app aimed to help you to manage your grocery list"
-        />
-        <meta
-          name="keywords"
-          content="grocery, list, shopping, management, food"
-        />
-      </Head>
       <body style={{ margin: 0 }}>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
